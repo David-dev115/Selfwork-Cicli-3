2cbaf8f405262f877648b9c972ec2ee6232de77c
David Paparo <d.paparo@tiscali.it>
2024-12-17 15:28:12 +0100

    eseguo correzioni con video Selfwork

--- main.js
+++ main.js
@@ -1,19 +1,62 @@
 
+// Metodo Switch
+
+let bevanda_1 = `Acqua`;
+let bevanda_2 = `Coca-cola`;
+let bevanda_3 = `Birra`;
+
+let scelta_x;
+
+do{
+    scelta_x = Number (prompt ( `Inserisci il nr corrispondente alla bevanda scelta \n 1 Acqua \n 2 Coca-cola \n 3 Birra`));
+
+    switch (scelta_x) {
+        case 1:
+            console.log(`Hai scelto ${bevanda_1}`);
+            break;
+
+        case 2:
+            console.log(`Hai scelto ${bevanda_2}`);
+            break;
+
+        case 3:
+            console.log(`Hai scelto ${bevanda_3}`);
+            break;
+    
+        default:
+            console.log(`scelta non valida`);
+            
+            break;
+    }
 
 
-let selezione_bevanda = prompt(`Indica il nr. corrispendente alla bevanda che desideri : \n 1 Acqua \n 2 Coca Cola \n 3 Birra`);
+}while ( scelta_x < 1 || scelta_x > 3)
 
 
-while ( selezione_bevanda != 1, selezione_bevanda != 2, selezione_bevanda != 3 ) {
 
-    selezione_bevanda = prompt ( `Selezione non valida : inserisci un numero tra 1 e 3` )
+// Metodo If/Else
 
-}
+// let bevanda_4 = `Spuma`;
 
+// let bevanda_5 = `Limonata`;
 
-if ( selezione_bevanda == 1) {
-    console.log( `E' stata selezionata l'acqua` );   
-} else if( selezione_bevanda == 2) {
-    console.log( `E' stata selezionata Coca-Cola` );
-} else { console.log( ` E' stata selezionata Birra` );
- }
+// let bevanda_6 = `Chinotto`;
+
+// let bevanda_scelta;
+
+// do{
+//         bevanda_scelta = prompt ( ` Inserisci il nr corrispondente alla bevanda che desideri: \n 4 Spuma \n 5 Limonata \n 6 Chinotto` )
+
+//         if( bevanda_scelta == 4) {
+//                 console.log(`La bevanda scelta è ${bevanda_4}` );
+                
+//         }else if ( bevanda_scelta == 5 ){
+//                 console.log( `La bevanda scelta è ${bevanda_5}` );
+                
+//         }else if ( bevanda_scelta == 6 ) {
+//                 console.log( ` La bevanda scelta è ${bevanda_6}` );
+                
+//         } else { console.log( `Selezione non valida`);
+//         }
+
+// }while (bevanda_scelta < 4 || bevanda_scelta> 6 ) 
